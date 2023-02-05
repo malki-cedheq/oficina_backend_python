ef1484f8f4ee51228adb22ab2dbf54e0fd42251e
malki-cedheq <malkicedheq@gmail.com>
2023-02-05 19:08:11 -0300

    minor change

--- ex16_fastapi/main_test.py
+++ ex16_fastapi/main_test.py
@@ -11,6 +11,13 @@ client = TestClient(app)
 
 
 def test_index():
+    '''Teste Positivo'''
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello Wordl"}
+
+
+def test_login():
+    '''Teste Negativo (não existe endpoint /login'''
+    response = client.get("/login")
+    assert response.status_code == 403

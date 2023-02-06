841a60b870d3883e98dff14dc75a3b8b5bc998c1
malki-cedheq <malkicedheq@gmail.com>
2023-02-06 13:52:31 -0300

    update ex11_flask

--- ex11_flask/app.py
+++ ex11_flask/app.py
@@ -4,15 +4,54 @@ Serialização e Desserialização
 --Sugerido Flask-Marshmallow
 '''
 
-from flask import Flask, jsonify
+from flask import Flask
+from flask_marshmallow import Marshmallow
 
 app = Flask(__name__)
+ma = Marshmallow(app)
 
 
-@app.route("/", methods=["GET"])
-def home():
-    dicionario = {"message": "Hello Word", "valor": 100, "boolean": True}
-    return jsonify(dicionario)  # serialização
+usuarios = [
+    {"user_id": 1, "username": "jose",
+        "email": "jose@email.com", "password": "1f2f3f"},
+    {"user_id": 2, "username": "pedro",
+        "email": "pedro@email.com", "password": "1e2e3e"},
+    {"user_id": 3, "username": "marcos",
+        "email": "marcos@email.com", "password": "1d2d3d"}
+]
+
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("username", "email")  # campos expostos
+
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
+
+
+@app.route("/usuarios", methods=["GET"])
+def users() -> list:
+    ''' Retorna lista com todos os usuários'''
+    all_users = usuarios
+    return users_schema.dump(all_users)
+
+
+@app.route("/usuarios/<int:user_id>", methods=["GET"])
+def user_detail(user_id: int) -> dict:
+    ''' Retorna apenas o usuário a qual pertence o identificador'''
+
+    # utilizando list comprehension, retorna dict
+    user = list(users for users in usuarios if users['user_id'] == user_id)[0]
+
+    # utilizando filter, retorna dict
+    #user = list(filter(lambda usuarios: usuarios['user_id'] == user_id, usuarios))[0]
+
+    # utilizando next, retorna dict
+    #user = next((users for users in usuarios if users['user_id'] == user_id), {})
+
+    print(user)
+    return user_schema.dump(user)
 
 
 if __name__ == "__main__":

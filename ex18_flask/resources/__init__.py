019d6fbc412a77c0ef1a5c680a7f60cc263fde01
malki-cedheq <malkicedheq@gmail.com>
2023-02-21 01:00:12 -0300

    add blueprint setup

--- ex18_flask/resources/__init__.py
+++ ex18_flask/resources/__init__.py
@@ -1,11 +1,12 @@
 # resources/__init__.py
-
+from flask import Blueprint
 from flask_restx import Api, apidoc
 
-from .item import api as ns1
+from .item import ns_item
+from .thing import ns_thing
 
 api = Api(title='API Restful', version='1.0',
-          description='API Restful com flask-restx', doc='/doc/')
+          description='API Restful com flask-restx', doc='/api/doc/')
 
 
 @api.documentation
@@ -13,4 +14,5 @@ def custom_ui():
     return apidoc.ui_for(api)
 
 
-api.add_namespace(ns1, path='/api')
+api.add_namespace(ns_item, path='/api')
+api.add_namespace(ns_thing, path='/api')
